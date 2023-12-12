d6c08db5a29f807a897d7f568f1588869fe76162
vinod <vinod.khetade@pharmarack.com>
2023-12-12 23:46:28 +0530

    Update delcare_Var.go

--- golearn/delcare_Var.go
+++ golearn/delcare_Var.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+const PI = 3.14
+
 func main() {
 	var student1 string = "John" //type is string
 	var student2 = "Jane"        //type is inferred
@@ -13,10 +15,14 @@ func main() {
 	fmt.Println(student2)
 	fmt.Println(x)
 
+	fmt.Println(" Int varable")
 	var a, b, c, d int = 1, 3, 5, 7
 
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
 	fmt.Println(d)
+	fmt.Println(" Const varable")
+	const PI = 3.142
+	fmt.Println(PI)
 }
