114f83255d0878c6b84bbfbf194bfcc355e59d90
vinod <vinod.khetade@pharmarack.com>
2023-12-12 23:41:48 +0530

    Create delcare_Var.go

---
+++ golearn/delcare_Var.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var student1 string = "John" //type is string
+	var student2 = "Jane"        //type is inferred
+	x := 2                       //type is inferred
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+
+	var a, b, c, d int = 1, 3, 5, 7
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
