b675694b775f0c56c494d5f526cf4b19a4a587ba
vinod <vinod.khetade@pharmarack.com>
2023-12-12 23:17:20 +0530

    Update values.go

--- golearn/values.go
+++ golearn/values.go
@@ -3,7 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("go" + "lang")
-	fmt.Printf("Vinod bhaua")
+	fmt.Println("go" + "lang") // println add new line to the end
+	fmt.Println("Vinod bhaua")
+	// fmt.Printf("Hello go")
+
+	// add number
+	// A basic set of examples showing that %v is the default format, in this
+	// case decimal for integers, which can be explicitly requested with %d;
+	// the output is just what Println generates.
+	integer := 23
+	// Each of these prints "23" (without the quotes).
+	fmt.Println(integer)
+
+	fmt.Printf("%v\n", integer)
+	fmt.Printf("%d\n", integer)
 
 }
