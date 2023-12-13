98cfa41e71497e9b406899f11cde322af5c1e8f2
vinod <vinod.khetade@pharmarack.com>
2023-12-13 23:04:34 +0530

    array learn length

--- golearn/array.go
+++ golearn/array.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func arraytest() {
+func main() {
 	var arr1 = [3]int{1, 2, 3}
 	arr2 := [5]int{4, 5, 6, 7, 8}
 
@@ -12,8 +12,15 @@ func arraytest() {
 	fmt.Println(arr2)
 
 	var cars = [...]string{"Volvo", "BMW", "Ford", "Mazda"}
-	fmt.Print(cars)
+	fmt.Println(cars)
 	car2 := [...]string{"dd", "aa"}
-	fmt.Print(car2)
+	fmt.Println(car2)
+	// 	1:10 means: assign 10 to array index 1 (second element).
+	// 2:40 means: assign 40 to array index 2 (third element).
+
+	arr3 := [...]int{1: 10, 2: 40}
+
+	fmt.Println(arr3)
+	fmt.Println("Length of array 3", len(arr3))
 
 }
