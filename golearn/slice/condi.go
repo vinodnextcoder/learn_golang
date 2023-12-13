31d33fbd7f526669e72ba7853b7360b0e744e61e
vinod <vinod.khetade@pharmarack.com>
2023-12-13 23:20:13 +0530

    condition added

--- golearn/slice/condi.go
+++ golearn/slice/condi.go
@@ -5,9 +5,13 @@ import (
 )
 
 func main() {
-	x := 20
-	y := 18
-	if x > y {
-		fmt.Println("x is greater than y")
+	num := 20
+	if num >= 10 {
+		fmt.Println("Num is more than 10.")
+		if num > 15 {
+			fmt.Println("Num is also more than 15.")
+		}
+	} else {
+		fmt.Println("Num is less than 10.")
 	}
 }
