21fac28e739c62b5af71dfc81009a368106b0596
vinod <vinodk2757@gmail.com>
2024-01-24 09:48:23 +0530

    update query

---
+++ gorout/chanel/channels.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+// Channels are the pipes that connect concurrent goroutines. 
+// You can send values into channels from one goroutine 
+// and receive those values into another goroutine.
+
+
+func main() {
+
+    messages := make(chan string)
+
+    go func() { messages <- "ping" }()
+
+    msg := <-messages
+    fmt.Println(msg)
+}
